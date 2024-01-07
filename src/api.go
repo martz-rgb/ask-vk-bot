dd4b455ebcdb19fc9e5f919c8a37d66c1415692d
noise <noise.martz@gmail.com>
2024-01-07 18:10:27 +0300

    change keyboard through edit + remove mutex from VkApi

--- src/api.go
+++ src/api.go
@@ -3,17 +3,15 @@ package main
 import (
 	"math/rand"
 	"strings"
-	"sync"
 	"time"
 
 	"github.com/SevereCloud/vksdk/v2/api"
+	"github.com/SevereCloud/vksdk/v2/object"
 	"github.com/awnumar/memguard"
 	"go.uber.org/zap"
 )
 
 type VkApi struct {
-	sync.Mutex
-
 	r *rand.Rand
 
 	// api.VK is based on http.Client and http.Client is claimed to be concurrency safe
@@ -35,9 +33,6 @@ func NewVkApi(group_token *memguard.LockedBuffer, admin_token *memguard.LockedBu
 }
 
 func (a *VkApi) MarkAsRead(user_id int) {
-	a.Lock()
-	defer a.Unlock()
-
 	params := api.Params{
 		"mark_conversation_as_read": true,
 		"peer_id":                   user_id,
@@ -58,9 +53,6 @@ func (a *VkApi) MarkAsRead(user_id int) {
 }
 
 func (a *VkApi) SendMessage(user_id int, message string, keyboard string) int {
-	a.Lock()
-	defer a.Unlock()
-
 	params := api.Params{
 		"user_id":   user_id,
 		"random_id": a.r.Int(),
@@ -84,10 +76,29 @@ func (a *VkApi) SendMessage(user_id int, message string, keyboard string) int {
 	return response
 }
 
-func (a *VkApi) DeleteMessage(peer_id int, message_id int, delete_for_all int) {
-	a.Lock()
-	defer a.Unlock()
+func (a *VkApi) EditMessage(peer_id int, message_id int, message string, keyboard string) {
+	params := api.Params{
+		"peer_id":    peer_id,
+		"message_id": message_id,
+		"message":    message,
+		"keyboard":   keyboard,
+	}
 
+	response, err := a.group.MessagesEdit(params)
+	if err != nil {
+		zap.S().Errorw("failed to edit vk message",
+			"error", err,
+			"params", params,
+			"response", response)
+		return
+	}
+
+	zap.S().Debugw("successfully edited vk message",
+		"params", params,
+		"response", response)
+}
+
+func (a *VkApi) DeleteMessage(peer_id int, message_id int, delete_for_all int) {
 	params := api.Params{
 		"peer_id":        peer_id,
 		"message_ids":    message_id,
@@ -108,9 +119,6 @@ func (a *VkApi) DeleteMessage(peer_id int, message_id int, delete_for_all int) {
 }
 
 func (a *VkApi) SendEventAnswer(event_id string, user_id int, peer_id int) {
-	a.Lock()
-	defer a.Unlock()
-
 	params := api.Params{
 		"event_id":  event_id,
 		"user_id":   user_id,
@@ -132,7 +140,56 @@ func (a *VkApi) SendEventAnswer(event_id string, user_id int, peer_id int) {
 		"response", response)
 }
 
+func (a *VkApi) GetLastBotMessage(user_id int) *object.MessagesMessage {
+	params := api.Params{
+		"count":   20, // heuristic value
+		"user_id": user_id,
+	}
+
+	response, err := a.group.MessagesGetHistory(params)
+	if err != nil {
+		zap.S().Errorw("failed to get last bot message",
+			"error", err,
+			"params", params,
+			"response", response)
+		return nil
+	}
+
+	zap.S().Debugw("successfully got last messages",
+		"params", params,
+		"response", response)
+
+	for _, message := range response.Items {
+		if message.Out {
+			return &message
+		}
+	}
+
+	return nil
+}
+
 func (a *VkApi) ChangeKeyboard(user_id int, keyboard string) {
+	ok := a.ChangeKeyboardWithoutDelete(user_id, keyboard)
+
+	if ok == false {
+		a.ChangeKeyboardWithDelete(user_id, keyboard)
+
+		zap.S().Warnw("unable to change keyboard without delete")
+	}
+}
+
+func (a *VkApi) ChangeKeyboardWithDelete(user_id int, keyboard string) {
 	id := a.SendMessage(user_id, "Меняю клавиатуру", keyboard)
 	a.DeleteMessage(user_id, id, 1)
+}
+
+func (a *VkApi) ChangeKeyboardWithoutDelete(user_id int, keyboard string) bool {
+	message := a.GetLastBotMessage(user_id)
+	if message == nil {
+		return false
+	}
+
+	// vk allows edit any group's message somehow
+	a.EditMessage(user_id, message.ID, message.Text, keyboard)
+	return true
 }
