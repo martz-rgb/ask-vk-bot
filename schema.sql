08df33dea7ca310925d7b96b3d5f089e816977f4
noise <noise.martz@gmail.com>
2024-01-16 13:55:26 +0300

    expand schema

--- schema.sql
+++ schema.sql
@@ -1,3 +1,7 @@
+CREATE TABLE IF NOT EXISTS administration (
+    vk_id INT PRIMARY KEY NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS roles (
     name TEXT PRIMARY KEY NOT NULL,
     tag TEXT,
@@ -10,5 +14,15 @@ CREATE TABLE IF NOT EXISTS roles (
 CREATE TABLE IF NOT EXISTS persons (
     vk_id INT PRIMARY KEY NOT NULL,
     name TEXT,
-    points INT
+    gallery TEXT,
+    birthday TEXT
 );
+
+CREATE TABLE IF NOT EXISTS points (
+    id  INT PRIMARY KEY NOT NULL,
+    person INT REFERENCES persons(vk_id) NOT NULL,
+    diff INT,
+    cause TEXT,
+    timestamp TEXT
+);
+CREATE INDEX IF NOT EXISTS idx_points_person ON points(person);
