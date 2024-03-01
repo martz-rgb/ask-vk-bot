178652f05435a7807176e913bca9ddf0dd084dfe
noise <noise.martz@gmail.com>
2024-03-01 20:02:55 +0300

    dict extract deep and flatten

--- src/dict/dict.go
+++ src/dict/dict.go
@@ -9,16 +9,35 @@ import (
 
 type Dictionary map[string]interface{}
 
-func ExtractValue[T any](dict Dictionary, name string) (T, error) {
+func ExtractValue[T any](dict Dictionary, keys ...string) (T, error) {
+	var err error
+
+	if dict == nil {
+		err = errors.New("dictionary is nil")
+		return *new(T), zaperr.Wrap(err, "")
+	}
+
+	for i := 0; i < len(keys)-1; i++ {
+		dict, err = ExtractFlatten[Dictionary](dict, keys[i])
+		if err != nil {
+			return *new(T), err
+		}
+	}
+
+	return ExtractFlatten[T](dict, keys[len(keys)-1])
+}
+
+func ExtractFlatten[T any](dict Dictionary, key string) (T, error) {
 	if dict == nil {
 		err := errors.New("dictionary is nil")
 		return *new(T), zaperr.Wrap(err, "")
 	}
-	value, ok := dict[name]
+
+	value, ok := dict[key]
 	if !ok {
 		err := errors.New("no such key is presented in form")
 		return *new(T), zaperr.Wrap(err, "",
-			zap.Any("key", name),
+			zap.Any("key", key),
 			zap.Any("form", dict))
 	}
 
