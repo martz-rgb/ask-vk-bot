a2bc1850a9d84e76f5ea207f3acef59e156dcf12
noise <noise.martz@gmail.com>
2024-03-01 20:02:05 +0300

    form confirmation

---
+++ src/form_confirmation.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"ask-bot/src/ask"
+	"ask-bot/src/dict"
+	"ask-bot/src/vk"
+	"fmt"
+)
+
+func ConfirmReservationDeletion(values dict.Dictionary) (*vk.MessageParams, error) {
+	reservation, err := dict.ExtractValue[*ask.ReservationDetail](values, "reservation")
+	if err != nil {
+		return nil, err
+	}
+
+	return &vk.MessageParams{
+		Text: fmt.Sprintf("Вы уверены, что хотите удалить бронь на %s от @id%d?",
+			reservation.AccusativeName,
+			reservation.VkID),
+	}, nil
+}
